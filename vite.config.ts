9159bc48ea1906afdcb5bf582e5c315829476298
Anandreddy125 <108915667+Anandreddy125@users.noreply.github.com>
2025-01-07 15:10:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: 'src/main.tsx', // Correct relative path
+      input: 'src/main.tsx',
     },
   },
 });
